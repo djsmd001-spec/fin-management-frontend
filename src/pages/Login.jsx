47bec2f10f9c4349f67eb9e2608447408abd6d34
gorakh <djsmd001@gmail.com>
2026-02-14 20:13:05 +0530

    Fixed auth routes with /api

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -19,7 +19,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await API.post("/auth/login", form);
+      const res = await API.post("/api/auth/login", form);
 
       const { token, user } = res.data;
 
