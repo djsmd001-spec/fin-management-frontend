61e6ca9ccfa8b17b252a6e9f8d1d3be97d43ba97
gorakh <djsmd001@gmail.com>
2026-02-14 20:29:44 +0530

    Added token interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,15 @@ const API = axios.create({
   baseURL: "https://fin-management-backend-production.up.railway.app/api",
 });
 
+// 🔥 Add Token Automatically
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default API;
